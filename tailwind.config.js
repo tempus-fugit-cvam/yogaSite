f9742ffdb33a0a354e111a65b304375a6e924565
Vic <vfacory@gmail.com>
2021-08-13 21:42:01 +0100

    Create custom colours
    Changed colours in footer.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-dark-salmon': '#F09A7F',
+        'brand-melon': '#FDC0AF',
+        'brand-peach': '#F6DAA2',
+        'brand-medium-champagne': '#FBEBB1',
+        'brand-platinum': '#EBEAEB',
+        'brand-antique-white': '#FFEFDE',
+        'brand-deep-champagne': '#FED7AA',
+        'brand-gunmetal': '#1F2937',
+      },
+    },
   },
   variants: {
     extend: {},

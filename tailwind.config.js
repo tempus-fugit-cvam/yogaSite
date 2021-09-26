b3e50749b895fff2653bfc1e80b18a5825b55653
mafromist <mafromist@gmail.com>
2021-09-26 17:23:18 +0100

    Timetable responsiveness added

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,5 +13,22 @@ module.exports = {
         'brand-gunmetal': '#1F2937',
       },
     },
+
+    screens: {
+      'sm': '360px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
 }
